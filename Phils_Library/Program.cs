d105776cc7ff8d41a7178aa4ede60c24b34495bf
Olamide Ogunde <oogunde0413@gmail.com>
2020-07-15 13:21:16 -0700

    Add project files.

---
+++ Phils_Library/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Phils_Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
